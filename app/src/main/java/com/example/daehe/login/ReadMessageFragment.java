a391224ca5f91f9f5b446eab6a8e5a89292d8180
daehee-kang <daehee6316@gmail.com>
2018-02-27 12:04:01 -0800

    message worked

---
+++ app/src/main/java/com/example/daehe/login/ReadMessageFragment.java
@@ -0,0 +1,21 @@
+package com.example.daehe.login;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ReadMessageFragment extends Fragment {
+    View myView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView=inflater.inflate(R.layout.fragment_read_message,container,false);
+        return myView;
+    }
+}
